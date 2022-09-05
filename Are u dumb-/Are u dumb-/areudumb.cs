b143bd1e1221d9bc5e31e7f5d875e2f0241110ce
RossoDev <44946921+RossoDev@users.noreply.github.com>
2022-09-05 23:11:01 +0200

    Update areudumb.cs
    explanations

--- Are u dumb-/Are u dumb-/areudumb.cs
+++ Are u dumb-/Are u dumb-/areudumb.cs
@@ -13,18 +13,22 @@ namespace Are_u_dumb_
 
         private void areudumb_Load(object sender, EventArgs e)
         {
-            //
+            // in startup
         }
 
         private void btnyes_Click(object sender, EventArgs e)
         {
+            // button click "yes" event
             lblareudumb.Text = "I knew it! :3";
+
+            // buttons hide
             btnyes.Hide();
             btnno.Hide();
         }
 
         private void btnno_Click(object sender, EventArgs e)
         {
+            // generate random coordinates to position the button
             Random rnd = new Random();
 
             int x = rnd.Next(200, 270);
